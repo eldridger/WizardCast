565932f5c166b6ec8993fb15bbc2f3fc54a8a364
eldridger <eldridger@wit.edu>
2014-02-27 21:36:08 -0500

    first

---
+++ app/src/main/java/com/wizardcast/MainActivity.java
@@ -0,0 +1,55 @@
+package com.wizardcast;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    private GLSurfaceView glSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //turn off window title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+
+        // Fullscreen mode
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //create menu surface
+        //glSurfaceView = new menuView(this);
+
+
+        setContentView(R.layout.main_menu);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
